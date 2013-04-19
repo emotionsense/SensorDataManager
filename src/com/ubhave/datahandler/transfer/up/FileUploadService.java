92b6a5618d5aee5b1c0a01baf6e22b41e603dd6d
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-04-19 12:38:49 +0100

    Re-organise structure

--- src/com/ubhave/datahandler/transfer/up/FileUploadService.java
+++ src/com/ubhave/datahandler/transfer/up/FileUploadService.java
@@ -20,7 +20,7 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
 IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  ************************************************** */
 
-package com.ubhave.datahandler.transfer;
+package com.ubhave.datahandler.transfer.up;
 
 
 // OLD CODE >> REFERENCE ONLY
