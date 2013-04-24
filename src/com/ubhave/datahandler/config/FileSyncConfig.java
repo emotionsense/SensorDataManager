7797a7fbf171ac8e55b8cdaee6f632c33a0dd858
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-04-24 18:40:49 +0100

    Cleaning up config

--- src/com/ubhave/datahandler/config/FileSyncConfig.java
+++ src/com/ubhave/datahandler/config/FileSyncConfig.java
@@ -15,9 +15,6 @@ public class FileSyncConfig
 	
 	public final static String RESPONSE_DATE_MODIFIED_KEY = "responseDateModified";
 	public final static String SYNC_FREQUENCY = "syncFrequency";
-	
-	// Sync Policy Unimplemented
-//	public final static String SYNC_CONNECTION_TYPE = "syncConnectionType";
 
 	/*
 	 * Config Values
@@ -26,7 +23,7 @@ public class FileSyncConfig
 	public final static String DEFAULT_REQUEST_DATE_MODIFIED = "date_modified";
 	public final static String DEFAULT_REQUEST_GET_FILE = "get_file";
 	public final static String DEFAULT_RESPONSE_DATE_MODIFIED = DEFAULT_REQUEST_DATE_MODIFIED;
-	public final static long DEFAULT_SYNC_FREQUENCY = 1000 * 60 * 60 * 24; // daily
+	public final static long DEFAULT_DAILY_SYNC_FREQUENCY = 1000 * 60 * 60 * 24;
 	
 	public static HashSet<String> validKeys()
 	{
@@ -46,8 +43,7 @@ public class FileSyncConfig
 		defaults.put(REQUEST_DATE_MODIFIED_VALUE, DEFAULT_REQUEST_DATE_MODIFIED);
 		defaults.put(REQUEST_GET_FILE_VALUE, DEFAULT_REQUEST_GET_FILE);
 		defaults.put(RESPONSE_DATE_MODIFIED_KEY, DEFAULT_RESPONSE_DATE_MODIFIED);
-		defaults.put(SYNC_FREQUENCY, DEFAULT_SYNC_FREQUENCY);
+		defaults.put(SYNC_FREQUENCY, DEFAULT_DAILY_SYNC_FREQUENCY);
 		return defaults;
 	}
-	
 }
