c601ef50e517b35e444aa31964b47e89796a7ac7
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-05-13 16:33:55 +0100

    Revert "Adding sync frequency config"
    This reverts commit e4f4da268faeb4e2b5a5e7f11b3fb47cd617a564.

--- src/com/ubhave/datahandler/config/FileSyncConfig.java
+++ src/com/ubhave/datahandler/config/FileSyncConfig.java
@@ -14,7 +14,6 @@ public class FileSyncConfig
 	public final static String REQUEST_GET_FILE_VALUE = "requestGetFile";
 	
 	public final static String RESPONSE_DATE_MODIFIED_KEY = "responseDateModified";
-	public final static String SYNC_FREQUENCY = "syncFrequency";
 	
 	// Sync Policy Unimplemented
 //	public final static String SYNC_CONNECTION_TYPE = "syncConnectionType";
@@ -26,7 +25,6 @@ public class FileSyncConfig
 	public final static String DEFAULT_REQUEST_DATE_MODIFIED = "date_modified";
 	public final static String DEFAULT_REQUEST_GET_FILE = "get_file";
 	public final static String DEFAULT_RESPONSE_DATE_MODIFIED = DEFAULT_REQUEST_DATE_MODIFIED;
-	public final static long DEFAULT_SYNC_FREQUENCY = 1000 * 60 * 60 * 24; // daily
 	
 	public static HashSet<String> validKeys()
 	{
@@ -35,7 +33,6 @@ public class FileSyncConfig
 		validKeys.add(REQUEST_DATE_MODIFIED_VALUE);
 		validKeys.add(REQUEST_GET_FILE_VALUE);
 		validKeys.add(RESPONSE_DATE_MODIFIED_KEY);
-		validKeys.add(SYNC_FREQUENCY);
 		return validKeys;
 	}
 	
@@ -46,7 +43,6 @@ public class FileSyncConfig
 		defaults.put(REQUEST_DATE_MODIFIED_VALUE, DEFAULT_REQUEST_DATE_MODIFIED);
 		defaults.put(REQUEST_GET_FILE_VALUE, DEFAULT_REQUEST_GET_FILE);
 		defaults.put(RESPONSE_DATE_MODIFIED_KEY, DEFAULT_RESPONSE_DATE_MODIFIED);
-		defaults.put(SYNC_FREQUENCY, DEFAULT_SYNC_FREQUENCY);
 		return defaults;
 	}
 	
