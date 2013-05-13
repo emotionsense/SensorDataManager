8f4f3f7ae07f14fad1235704e196c03ab6f399d9
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-05-13 16:33:51 +0100

    Revert "Adding default root directory"
    This reverts commit e67d478db511a59d21dfb92c34a273711bf9a7e2.

--- src/com/ubhave/datahandler/except/DataHandlerException.java
+++ src/com/ubhave/datahandler/except/DataHandlerException.java
@@ -8,7 +8,6 @@ public class DataHandlerException extends Exception
 	public final static int NO_URL_TARGET = 11;
 	public final static int IO_EXCEPTION = 12;
 	public final static int UNIMPLEMENTED = 13;
-	public final static int WRITING_TO_DEFAULT_DIRECTORY = 14;
 	
 	private final int errorCode;
 	
