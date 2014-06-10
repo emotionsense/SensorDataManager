92b7d70d44c810fc55af36b3044fdf6b5bb19285
Hugues SALAMIN <hugues.salamin@idiap.ch>
2014-06-10 11:55:57 +0200

    Set locale for date formating in JSON to Locale.us
    This ensure the data is in the same format, no matter what the phone
    locale is

--- src/com/ubhave/dataformatter/json/JSONFormatter.java
+++ src/com/ubhave/dataformatter/json/JSONFormatter.java
@@ -24,6 +24,7 @@ package com.ubhave.dataformatter.json;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -48,11 +49,11 @@ public abstract class JSONFormatter extends DataFormatter
 	private final static String UNKNOWN_SENSOR = "unknownSensor";
 	private final static String USER_ID = "userid";
 	private final static String DEVICE_ID = "deviceid";
-	
+
 	protected final Context applicationContext;
 	protected final int sensorType;
 	protected final DataHandlerConfig config;
-	
+
 	public JSONFormatter(Context c, int sensorType)
 	{
 		applicationContext = c;
@@ -73,8 +74,7 @@ public abstract class JSONFormatter extends DataFormatter
 				SensorConfig config = data.getSensorConfig();
 				addGenericConfig(json, config);
 				addSensorSpecificConfig(json, config);
-			}
-			catch (JSONException e)
+			} catch (JSONException e)
 			{
 				e.printStackTrace();
 			}
@@ -95,7 +95,7 @@ public abstract class JSONFormatter extends DataFormatter
 			return null;
 		}
 	}
-	
+
 	public long getTimestamp(String sensorDataJsonString)
 	{
 		long timestamp = 0;
@@ -106,89 +106,88 @@ public abstract class JSONFormatter extends DataFormatter
 		}
 		return timestamp;
 	}
-	
+
 	protected JSONObject parseData(String jsonString)
 	{
 		try
 		{
 			return new JSONObject(jsonString);
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	protected<T> ArrayList<T> getJSONArray(JSONObject data, String key, Class<T> c) throws NullPointerException
+
+	protected <T> ArrayList<T> getJSONArray(JSONObject data, String key,
+			Class<T> c) throws NullPointerException
 	{
 		try
 		{
 			ArrayList<T> list = new ArrayList<T>();
 			JSONArray jsonArray = (JSONArray) data.get(key);
-			for (int i=0; i<jsonArray.length(); i++)
+			for (int i = 0; i < jsonArray.length(); i++)
 			{
 				try
 				{
 					T member = c.cast(jsonArray.get(i));
 					list.add(member);
-				}
-				catch (Exception e)
+				} catch (Exception e)
 				{
 					e.printStackTrace();
 				}
 			}
 			return list;
-		}
-		catch (JSONException e)
+		} catch (JSONException e)
 		{
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	@SuppressLint("SimpleDateFormat")
+	// @SuppressLint("SimpleDateFormat")
 	protected void addGenericData(JSONObject json, SensorData data)
 	{
 		try
 		{
-			String userId = (String) config.get(DataStorageConfig.UNIQUE_USER_ID);
+			String userId = (String) config
+					.get(DataStorageConfig.UNIQUE_USER_ID);
 			if (userId != null && userId.length() > 0)
 			{
 				json.put(USER_ID, userId);
 			}
-			
-			String deviceId = (String) config.get(DataStorageConfig.UNIQUE_DEVICE_ID);
+
+			String deviceId = (String) config
+					.get(DataStorageConfig.UNIQUE_DEVICE_ID);
 			if (deviceId != null && deviceId.length() > 0)
 			{
 				json.put(DEVICE_ID, deviceId);
 			}
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		
+
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(data.getTimestamp());
 
-		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss:SSS dd MM yyyy Z z");
+		SimpleDateFormat formatter = new SimpleDateFormat(
+				"HH:mm:ss:SSS dd MM yyyy Z z", Locale.US);
 		try
 		{
 			json.put(SENSE_TIME, formatter.format(calendar.getTime()));
 			json.put(SENSE_TIME_MILLIS, data.getTimestamp());
 			try
 			{
-				String sensorName = SensorUtils.getSensorName(data.getSensorType());
+				String sensorName = SensorUtils.getSensorName(data
+						.getSensorType());
 				json.put(SENSOR_TYPE, sensorName);
-			}
-			catch (ESException e)
+			} catch (ESException e)
 			{
 				e.printStackTrace();
 				json.put(SENSOR_TYPE, UNKNOWN_SENSOR);
 			}
-		}
-		catch (JSONException e)
+		} catch (JSONException e)
 		{
 			e.printStackTrace();
 		}
@@ -200,12 +199,12 @@ public abstract class JSONFormatter extends DataFormatter
 		try
 		{
 			String dateString = (String) json.get(SENSE_TIME);
-			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss:SSS dd MM yyyy Z z");
+			SimpleDateFormat formatter = new SimpleDateFormat(
+					"HH:mm:ss:SSS dd MM yyyy Z z");
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(formatter.parse(dateString));
 			return calendar.getTimeInMillis();
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			e.printStackTrace();
 			return -1;
@@ -214,53 +213,53 @@ public abstract class JSONFormatter extends DataFormatter
 
 	public abstract SensorData toSensorData(String jsonString);
 
-	protected abstract void addGenericConfig(JSONObject json, SensorConfig config) throws JSONException;
+	protected abstract void addGenericConfig(JSONObject json,
+			SensorConfig config) throws JSONException;
 
-	protected abstract void addSensorSpecificData(JSONObject json, SensorData data) throws JSONException;
+	protected abstract void addSensorSpecificData(JSONObject json,
+			SensorData data) throws JSONException;
 
-	protected abstract void addSensorSpecificConfig(JSONObject json, SensorConfig config) throws JSONException;
-	
+	protected abstract void addSensorSpecificConfig(JSONObject json,
+			SensorConfig config) throws JSONException;
+
 	protected abstract SensorConfig getGenericConfig(JSONObject json);
-	
+
 	protected Integer getInteger(String key, JSONObject data)
 	{
 		try
 		{
 			Integer value = ((Long) data.get(key)).intValue();
 			return value;
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			return null;
 		}
 	}
-	
+
 	protected String getString(String key, JSONObject data)
 	{
 		try
 		{
 			String value = (String) data.get(key);
 			return value;
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			return null;
 		}
 	}
-	
+
 	protected Boolean getBoolean(String key, JSONObject data)
 	{
 		try
 		{
 			Boolean value = (Boolean) data.get(key);
 			return value;
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			return null;
 		}
 	}
-	
+
 	protected Float getFloat(String key, JSONObject data)
 	{
 		try
@@ -270,9 +269,9 @@ public abstract class JSONFormatter extends DataFormatter
 			{
 				return value.floatValue();
 			}
-			else return null;
-		}
-		catch (Exception e)
+			else
+				return null;
+		} catch (Exception e)
 		{
 			return null;
 		}
