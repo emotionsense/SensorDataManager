d9db7082fa9f5914ae2e537c877cfefcb7035ced
nlathia <neal.lathia@cl.cam.ac.uk>
2016-01-08 00:06:32 +0100

    Dowgrade to support Android 4+

--- src/main/java/com/ubhave/datahandler/ESDataManagerInterface.java
+++ src/main/java/com/ubhave/datahandler/ESDataManagerInterface.java
@@ -1,37 +1,37 @@
 package com.ubhave.datahandler;
 
-import java.io.IOException;
-import java.util.List;
-
 import com.ubhave.dataformatter.DataFormatter;
 import com.ubhave.datahandler.except.DataHandlerException;
 import com.ubhave.datahandler.transfer.DataUploadCallback;
 import com.ubhave.sensormanager.ESException;
 import com.ubhave.sensormanager.data.SensorData;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface ESDataManagerInterface
 {
 	/*
 	 * Updating Data Manager config
 	 */
-	public void setConfig(final String key, final Object value) throws DataHandlerException;
-	public Object getConfig(final String key)  throws DataHandlerException;
+	void setConfig(final String key, final Object value) throws DataHandlerException;
+	Object getConfig(final String key)  throws DataHandlerException;
 	
 	/*
 	 * Logging/storing data
 	 */
-	public void logSensorData(final SensorData data) throws DataHandlerException;
-	public void logSensorData(final SensorData data, DataFormatter formatter) throws DataHandlerException;
-	public void logExtra(final String tag, final String data) throws DataHandlerException;
+	void logSensorData(final SensorData data) throws DataHandlerException;
+    void logSensorData(final SensorData data, DataFormatter formatter) throws DataHandlerException;
+	void logExtra(final String tag, final String data) throws DataHandlerException;
 	
 	/*
 	 * Retrieving logged data
 	 */
-	public List<SensorData> getRecentSensorData(int sensorId, long startTimestamp) throws DataHandlerException, ESException, IOException;
+	List<SensorData> getRecentSensorData(int sensorId, long startTimestamp) throws DataHandlerException, ESException, IOException;
 	
 	/*
 	 * Uploading stored data
 	 */
-	public void postAllStoredData(final DataUploadCallback callback) throws DataHandlerException;
+	void postAllStoredData(final DataUploadCallback callback) throws DataHandlerException;
 	
 }
--- src/main/java/com/ubhave/datahandler/transfer/async/UploadVaultInterface.java
+++ src/main/java/com/ubhave/datahandler/transfer/async/UploadVaultInterface.java
@@ -1,17 +1,17 @@
 package com.ubhave.datahandler.transfer.async;
 
-import java.io.File;
-import java.util.List;
+import com.ubhave.datahandler.except.DataHandlerException;
 
 import org.json.JSONObject;
 
-import com.ubhave.datahandler.except.DataHandlerException;
+import java.io.File;
+import java.util.List;
 
 public interface UploadVaultInterface
 {
-	public void writeData(final String dataName, final List<JSONObject> data) throws Exception;
+	void writeData(final String dataName, final List<JSONObject> data) throws Exception;
 	
-	public boolean isUploadDirectory(final File directory) throws DataHandlerException;
+	boolean isUploadDirectory(final File directory) throws DataHandlerException;
 	
-	public File getUploadDirectory() throws DataHandlerException;
+	File getUploadDirectory() throws DataHandlerException;
 }
